14d7dec39731320a91afc178b9196a76c3c67ef3
iamkryptonite <iamkryptonite@users.noreply.github.com>
2020-12-01 16:28:07 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Home from './components/Home';
+import Login from './components/Login'
+import Signup from './components/Signup'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/"><Home/></Route>
+          <Route path="/login"><Login/></Route>
+          <Route exact path="/signup"><Signup/></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Signup/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+class Signup extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Signup</h1>
+            </div>
+        )
+    }
+}
+export default Signup
