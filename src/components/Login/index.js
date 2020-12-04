853e7ca572cc390df1bc19040d26f8ca14859507
iamkryptonite <iamkryptonite@users.noreply.github.com>
2020-12-04 11:13:53 +0530

    added login and signup page

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,10 +1,61 @@
 import React from 'react';
 import './style.css';
+import LoginWall from './login-wall.jpg'
+import { Form, Input, Button, Checkbox } from 'antd';
+const layout = {
+  labelCol: { span: 5 },
+  wrapperCol: { span: 18 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 5, span: 18 },
+};
+ const onFinish = values => {
+    console.log('Success:', values);
+}
+const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+};
 class Login extends React.Component{
     render(){
         return(
-            <div>
-                <h1>Login</h1>
+            <div className="main-login">
+                <img src={LoginWall}></img>
+                <div className="form-login">
+                    <h1 className="login">Login</h1>
+                    <Form
+                    {...layout}
+                    name="basic"
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    >
+                        <Form.Item
+                            label="Username"
+                            name="username"
+                            rules={[{ required: true, message: 'Please input your username!' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Password"
+                            name="password"
+                            rules={[{ required: true, message: 'Please input your password!' }]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+
+                        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                            <Checkbox>Remember me</Checkbox>
+                        </Form.Item>
+
+                        <Form.Item {...tailLayout}>
+                            <Button type="primary" htmlType="submit">
+                            Login
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
             </div>
         )
     }
--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,10 +1,71 @@
 import React from 'react';
 import './style.css';
+import SignupWall from './signup-wall-2.jpg';
+import { Form, Input, Button, Checkbox } from 'antd';
+const layout = {
+  labelCol: { span: 5 },
+  wrapperCol: { span: 18 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 5, span: 20 },
+};
+ const onFinish = values => {
+    console.log('Success:', values);
+}
+const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+};
 class Signup extends React.Component{
     render(){
         return(
-            <div>
-                <h1>Signup</h1>
+            <div className="main">
+                <img src={SignupWall}></img>
+                <div className="form-signup">
+                    <h1 className="signup">Sign Up</h1>
+                    <Form
+                    {...layout}
+                    name="basic"
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    >
+                        <Form.Item
+                            label="Name"
+                            name="name"
+                            rules={[{ required: true, message: 'Please input your name!' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            label="Email"
+                            name="email"
+                            rules={[{ required: true, message: 'Please input your email!' }]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Password"
+                            name="password"
+                            rules={[{ required: true, message: 'Please input your password!' }]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+
+                        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                            <Checkbox>Remember me</Checkbox>
+                        </Form.Item>
+
+                        <Form.Item {...tailLayout}>
+                            <Button type="primary" htmlType="submit">
+                            Login
+                            </Button>
+                            <Button type="primary" htmlType="submit">
+                            Login
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </div>
             </div>
         )
     }
